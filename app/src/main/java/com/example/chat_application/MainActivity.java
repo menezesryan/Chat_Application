6da8ef5ef1e665cd02b598a21d87d60a692e5e2a
menezesryan <menezesryan17@gmail.com>
2021-11-01 19:34:54 +0530

    started making phone authentication

--- app/src/main/java/com/example/chat_application/MainActivity.java
+++ app/src/main/java/com/example/chat_application/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.chat_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button b;
+    private EditText phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.b);
+        phone = findViewById(R.id.phone);
     }
 }
