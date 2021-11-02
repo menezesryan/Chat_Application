6fd357d01719a2f8cb42c66b728e2c9bbea29f4c
menezesryan <menezesryan17@gmail.com>
2021-11-02 12:41:53 +0530

    created class for phone auth
    created a new class and xml file for phone auth.

--- app/src/main/java/com/example/chat_application/PhoneNumberAuth.java
+++ app/src/main/java/com/example/chat_application/PhoneNumberAuth.java
@@ -3,14 +3,12 @@ package com.example.chat_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class PhoneNumberAuth extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_phone_number_auth);
     }
 }
