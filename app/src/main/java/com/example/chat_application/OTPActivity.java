a4608b57fb83824c6df079f150cbfc522799039e
menezesryan <menezesryan17@gmail.com>
2021-11-02 13:01:18 +0530

    linked OTP and PhoneNumberAuth activity

---
+++ app/src/main/java/com/example/chat_application/OTPActivity.java
@@ -0,0 +1,14 @@
+package com.example.chat_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otpactivity);
+    }
+}
--- app/src/main/java/com/example/chat_application/PhoneNumberAuth.java
+++ app/src/main/java/com/example/chat_application/PhoneNumberAuth.java
@@ -2,13 +2,29 @@ package com.example.chat_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.chat_application.databinding.ActivityPhoneNumberAuthBinding;
 
 public class PhoneNumberAuth extends AppCompatActivity {
+
+    ActivityPhoneNumberAuthBinding binding;     //binding acts in place of findViewById(). so we can directly use xml id instead of creating objects here
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number_auth);
+        binding = ActivityPhoneNumberAuthBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PhoneNumberAuth.this, OTPActivity.class);
+                intent.putExtra("number", binding.phoneNumber.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
