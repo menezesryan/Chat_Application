52b8d39bf97400c8e96be4a06e751d4e251b7d66
menezesryan <menezesryan17@gmail.com>
2021-11-21 11:09:39 +0530

    setup profile activity backend made fucntional implemented the backend for setup profile activity. Pushes profile picture to storage, user details to the database. User interface still buggy. Need to rectify lot of things

--- app/src/main/java/com/example/chat_application/SetupProfileActivity.java
+++ app/src/main/java/com/example/chat_application/SetupProfileActivity.java
@@ -1,16 +1,27 @@
 package com.example.chat_application;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.chat_application.databinding.ActivitySetupProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SetupProfileActivity extends AppCompatActivity {
 
@@ -18,6 +29,8 @@ public class SetupProfileActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseDatabase database;
     FirebaseStorage storage;
+    Uri selectedImage;
+    ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +53,61 @@ public class SetupProfileActivity extends AppCompatActivity {
                 startActivityForResult(intent,1);
             }
         });
+
+        binding.setupProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = binding.nameBox.getText().toString();
+
+                if(name.isEmpty())
+                {
+                    binding.nameBox.setError("Please type a name");
+                    return;
+                }
+                if(selectedImage!=null)
+                {
+                    StorageReference reference = storage.getReference().child("Profiles").child(auth.getUid());
+                    reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful()){
+                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String imageUrl = uri.toString();
+                                        String uid = auth.getUid();
+                                        String phone = auth.getCurrentUser().getPhoneNumber();
+                                        String name = binding.nameBox.getText().toString();
+                                        User user = new User(uid,name,phone,imageUrl);
+                                        Toast.makeText(getApplicationContext(), "uploaded to storage", Toast.LENGTH_SHORT).show();
+                                        database.getReference()
+                                                .child("users")
+                                                .child(auth.getUid())
+                                                .setValue(user)
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(getApplicationContext(), "failed database", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                })
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void unused) {
+                                                        Intent intent = new Intent(SetupProfileActivity.this, MainActivity.class);
+                                                        Toast.makeText(getApplicationContext(), "uploaded to database", Toast.LENGTH_SHORT).show();
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                });
+
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
@@ -49,7 +117,12 @@ public class SetupProfileActivity extends AppCompatActivity {
         if(resultCode == RESULT_OK)
         {
             if(data.getData()!=null)
+            {
                 binding.imageView.setImageURI(data.getData());
+                selectedImage = data.getData();
+            }
+
+
         }
 
     }
