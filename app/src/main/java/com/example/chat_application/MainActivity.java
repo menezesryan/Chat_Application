dda8ef5a4780706952e752c03a13bea71ef58afa
sachin <sachin17120@gmail.com>
2021-12-12 13:06:00 +0530

    PULL test

--- app/src/main/java/com/example/chat_application/MainActivity.java
+++ app/src/main/java/com/example/chat_application/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //sachin
+        //PULL req test
     }
 }
